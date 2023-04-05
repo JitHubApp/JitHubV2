443887e153cf680b7e1bf0d369d9d82bfb355889
nerocui <nerocui@outlook.com>
2023-04-05 01:43:42 -0700

    Nero/features/repo sidebar (#4)
    * added segmented control for repo sidebar
    
    * changed the name of props to avoid confusion

--- JitHub/ViewModels/RepositoryViewModels/RepoSideBarViewModel.cs
+++ JitHub/ViewModels/RepositoryViewModels/RepoSideBarViewModel.cs
@@ -9,72 +9,112 @@ using CommunityToolkit.Mvvm.Input;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using JitHub.Models.Base;
+using Windows.UI.Xaml.Controls;
+using CommunityToolkit.Labs.WinUI;
 
-namespace JitHub.ViewModels.RepositoryViewModels
+namespace JitHub.ViewModels.RepositoryViewModels;
+
+public partial class RepoSideBarViewModel : RepoListViewModel<RepoModel>
 {
-    public class RepoSideBarViewModel : RepoListViewModel<RepoModel>
+    private ModalService _modalService;
+    private NavigationService _navigationService;
+    [ObservableProperty]
+    private int _selectedIndex;
+    [ObservableProperty]
+    private RepoType _repositoryPublicity;
+
+    public FeatureService FeatureService;
+    public RepoSideBarViewModel()
     {
-        private ModalService _modalService;
-        private NavigationService _navigationService;
+        _modalService = Ioc.Default.GetService<ModalService>();
+        _navigationService = Ioc.Default.GetService<NavigationService>();
+        FeatureService = Ioc.Default.GetService<FeatureService>();
+    }
 
-        public FeatureService FeatureService;
-        public RepoSideBarViewModel()
-        {
-            _modalService = Ioc.Default.GetService<ModalService>();
-            _navigationService = Ioc.Default.GetService<NavigationService>();
-            FeatureService = Ioc.Default.GetService<FeatureService>();
-        }
+    public override async Task<ICollection<RepoModel>> GetRepos()
+    {
+        var repos = await GitHubService.GetAllRepos();
+        var repoModels = repos
+            .Select(repo => new RepoModel(repo))
+            .ToList();
+        return repoModels;
+    }
 
-        public override async Task<ICollection<RepoModel>> GetRepos()
-        {
-            var repos = await GitHubService.GetAllRepos();
-            var repoModels = repos
-                .Select(repo => new RepoModel(repo))
-                .ToList();
-            return repoModels;
-        }
+    public void OnNewRepo()
+    {
+        _modalService.Open("New Repository", new RepoForm(new AsyncRelayCommand(LoadRepos)));
+    }
 
-        public void OnNewRepo()
-        {
-            _modalService.Open("New Repository", new RepoForm(new AsyncRelayCommand(LoadRepos)));
-        }
+    private void CloseModal()
+    {
+        _modalService?.Close();
+    }
 
-        private void CloseModal()
+    public async Task BuyProFeature()
+    {
+        var res = await FeatureService.BuyProFeature();
+        _modalService.Close();
+        switch (res)
         {
-            _modalService?.Close();
+            // TODO: Add reactions to all these cases
+            case FeaturePurchaseState.Success:
+                _modalService.Open("Thank you!", new ProLicensePurchaseSuccessDialog(new RelayCommand(CloseModal)));
+                break;
+            case FeaturePurchaseState.Failure:
+                break;
+            case FeaturePurchaseState.AlreadyOwn:
+                break;
+            default:
+                break;
         }
+    }
 
-        public async Task BuyProFeature()
+    public void OnManageRepo()
+    {
+        if (FeatureService.ProLicense)
         {
-            var res = await FeatureService.BuyProFeature();
-            _modalService.Close();
-            switch (res)
-            {
-                // TODO: Add reactions to all these cases
-                case FeaturePurchaseState.Success:
-                    _modalService.Open("Thank you!", new ProLicensePurchaseSuccessDialog(new RelayCommand(CloseModal)));
-                    break;
-                case FeaturePurchaseState.Failure:
-                    break;
-                case FeaturePurchaseState.AlreadyOwn:
-                    break;
-                default:
-                    break;
-            }
+            _navigationService.NavigateTo("Manage Repository", typeof(RepoManagePage));
         }
+        else
+        {
+            var buyCommand = new AsyncRelayCommand(BuyProFeature);
+            var cancelCommand = new RelayCommand(CloseModal);
+            _modalService.Open(new FeaturePurchaseDialog(buyCommand, cancelCommand));
+        }
+    }
 
-        public void OnManageRepo()
+    public void SegmentedSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        var segmentedControl = sender as Segmented;
+        var selectedIndex = segmentedControl.SelectedIndex;
+        RepositoryPublicity = (selectedIndex) switch
         {
-            if (FeatureService.ProLicense)
-            {
-                _navigationService.NavigateTo("Manage Repository", typeof(RepoManagePage));
-            }
-            else
-            {
-                var buyCommand = new AsyncRelayCommand(BuyProFeature);
-                var cancelCommand = new RelayCommand(CloseModal);
-                _modalService.Open(new FeaturePurchaseDialog(buyCommand, cancelCommand));
-            }
-        }
+            0 => RepoType.Public,
+            1 => RepoType.Private,
+            2 => RepoType.Forked,
+            _ => RepoType.Public,
+        };
+    }
+
+    public override bool IsForked(RepoModel repo)
+    {
+        return repo.Repository.Fork;
+    }
+
+    public override bool IsPrivate(RepoModel repo)
+    {
+        return repo.Repository.Private;
+    }
+
+    public override bool IsPublic(RepoModel repo)
+    {
+        return !repo.Repository.Private;
+    }
+
+    public override ICollection<RepoModel> NewRepoList()
+    {
+        return new List<RepoModel>();
     }
 }
--- JitHub/Views/Controls/RepoSideBar.xaml.cs
+++ JitHub/Views/Controls/RepoSideBar.xaml.cs
@@ -1,14 +1,11 @@
 ﻿using Windows.UI.Xaml.Controls;
 
-// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+namespace JitHub.Views.Controls;
 
-namespace JitHub.Views.Controls
+public sealed partial class RepoSideBar : UserControl
 {
-    public sealed partial class RepoSideBar : UserControl
+    public RepoSideBar()
     {
-        public RepoSideBar()
-        {
-            this.InitializeComponent();
-        }
+        this.InitializeComponent();
     }
 }
