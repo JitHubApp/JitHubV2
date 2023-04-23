63d25bbb2ad741141260fbe7e34ed47d5b7ddaa2
Bill Chen <billcshto@gmail.com>
2023-04-23 14:31:46 -0400

    clean up

--- JitHub.AuthFunction/GithubAuth.cs
+++ JitHub.AuthFunction/GithubAuth.cs
@@ -7,76 +7,72 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Octokit;
-using System.Text.Json;
-using Microsoft.AspNetCore.Mvc.Formatters.Internal;
 
-namespace JitHub.AuthFunction
+namespace JitHub.AuthFunction;
+public static class GithubAuth
 {
-    public static class GithubAuth
+    public static async Task<OauthToken> Detokenize(string code)
     {
-        public static async Task<OauthToken> Detokenize(string code)
+
+        try
         {
+            string clientId = Environment.GetEnvironmentVariable("JithubClientId");
+            string appSecret = Environment.GetEnvironmentVariable("JithubAppSecret");
 
-            try
-            {
-                string clientId = Environment.GetEnvironmentVariable("JithubClientId");
-                string appSecret = Environment.GetEnvironmentVariable("JithubAppSecret");
+            var request = new OauthTokenRequest(clientId, appSecret, code);
+            var gitHubClient = new GitHubClient(new ProductHeaderValue("JitHub"));
+            var token = await gitHubClient.Oauth.CreateAccessToken(request);
 
-                var request = new OauthTokenRequest(clientId, appSecret, code);
-                var gitHubClient = new GitHubClient(new ProductHeaderValue("JitHub"));
-                var token = await gitHubClient.Oauth.CreateAccessToken(request);
+            return token;
+        }
 
-                return token;
-            }
+        catch
+        {
+            return null;
+        }
 
-            catch
+    }
+
+    public static string ProcessRequest(HttpRequest req)
+    {
+        try
+        {
+            string temporaryCode = req.Query["code"];
+            temporaryCode = temporaryCode ?? req.Headers["code"];
+
+            if (String.IsNullOrWhiteSpace(temporaryCode))
             {
                 return null;
             }
-
+            return temporaryCode;
         }
 
-        public static string ProcessRequest(HttpRequest req)
+        catch
         {
-            try
-            {
-                string temporaryCode = req.Query["code"];
-                temporaryCode = temporaryCode ?? req.Headers["code"];
-
-                if (String.IsNullOrWhiteSpace(temporaryCode))
-                {
-                    return null;
-                }
-                return temporaryCode;
-            }
-
-            catch
-            {
-                return null;
-            }
+            return null;
         }
+    }
 
-        [FunctionName("GithubCodeToToken")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
-            ILogger log)
+    [FunctionName("GithubCodeToToken")]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+        ILogger log)
+    {
+
+        string code = ProcessRequest(req);
+
+        if (String.IsNullOrWhiteSpace(code))
         {
-
-            string code = ProcessRequest(req);
-
-            if (String.IsNullOrWhiteSpace(code))
-            {
-                return new BadRequestObjectResult("Missing Github code");
-            }
-
-            var token = await Detokenize(code);
-
-            if (token == null)
-            {
-                return new BadRequestObjectResult("Bad Request, Github request error");
-            }
-
-            return new OkObjectResult(new { token = token });
+            return new BadRequestObjectResult("Missing Github code");
         }
+
+        var token = await Detokenize(code);
+
+        if (token == null)
+        {
+            return new BadRequestObjectResult("Bad Request, Github request error");
+        }
+
+        return new OkObjectResult(new { token = token });
     }
 }
