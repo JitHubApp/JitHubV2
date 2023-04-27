85942c670bff49482672ec4719e3eef9c207c5a8
nerocui <nerocui@outlook.com>
2023-04-26 21:55:23 -0700

    Nero/better auth home (#16)
    * added fluent UI dependency
    
    * some really bad looking UI...
    
    * adding the same gradient from JitHub's login page.
    
    * added some skeleton

--- JitHub.Web/Client/Program.cs
+++ JitHub.Web/Client/Program.cs
@@ -1,6 +1,7 @@
 using JitHub.Web;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Fast.Components.FluentUI;
 
 namespace JitHub.Web
 {
@@ -11,6 +12,7 @@ namespace JitHub.Web
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
+            builder.Services.AddFluentUIComponents();
 
             builder.Services.AddScoped(sp =>
                 new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
