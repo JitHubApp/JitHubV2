ffeabda57535b48c03500e3dea8f3dfabf2a4de2
nerocui <nerocui@outlook.com>
2023-04-30 14:39:33 -0700

    Nero/better auth (#22)
    * added skeleton
    
    * added local api prefix
    
    * deleted unused page
    
    * added api integration
    
    * Azure function to authorize Github code (#9)
    
    * azure function to get github token
    
    * clean up
    
    * fix req handling
    
    * remove unused func
    
    * fix spacing
    
    * return status code and use null or empty
    
    * change tempCode param to code
    
    * clean up
    
    * e2e expierence for better auth (#15)
    
    * added webutil
    
    * Nero/better auth home (#16)
    
    * added fluent UI dependency
    
    * some really bad looking UI...
    
    * adding the same gradient from JitHub's login page.
    
    * added some skeleton
    
    * Nero/better auth home (#17)
    
    * some really bad looking UI...
    
    * using correct font
    
    * Nero/better auth home (#18)
    
    * another push
    
    * some finishing touch
    
    * removed fluent ui, added some polish (#19)
    
    * Nero/better auth home (#20)
    
    * removed fluent ui, added some polish
    
    * new screenshots
    
    * authorization page (#21)
    
    * clean up css
    
    * added dev mode to vs code
    
    ---------
    
    Co-authored-by: Ze C <billcshto@gmail.com>

---
+++ JitHub.AuthFunction/GithubAuth.cs
@@ -0,0 +1,83 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Octokit;
+
+namespace JitHub.AuthFunction;
+public static class GithubAuth
+{
+    public static async Task<OauthToken> Detokenize(string code)
+    {
+
+        try
+        {
+            string clientId = Environment.GetEnvironmentVariable("JithubClientId");
+            string appSecret = Environment.GetEnvironmentVariable("JithubAppSecret");
+
+            var request = new OauthTokenRequest(clientId, appSecret, code);
+            var gitHubClient = new GitHubClient(new ProductHeaderValue("JitHub"));
+            var token = await gitHubClient.Oauth.CreateAccessToken(request);
+
+            if (token == null || string.IsNullOrWhiteSpace(token.AccessToken) || !string.IsNullOrWhiteSpace(token.Error))
+            {
+                return null;
+            }
+
+            return token;
+        }
+
+        catch
+        {
+            return null;
+        }
+
+    }
+
+    public static string ProcessRequest(HttpRequest req)
+    {
+        try
+        {
+            string temporaryCode = req.Query["code"];
+            temporaryCode = temporaryCode ?? req.Headers["code"];
+
+            if (String.IsNullOrWhiteSpace(temporaryCode))
+            {
+                return null;
+            }
+            return temporaryCode;
+        }
+
+        catch
+        {
+            return null;
+        }
+    }
+
+    [FunctionName("GithubCodeToToken")]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+        ILogger log)
+    {
+
+        string code = ProcessRequest(req);
+
+        if (String.IsNullOrWhiteSpace(code))
+        {
+            return new BadRequestObjectResult("Missing Github code");
+        }
+
+        var token = await Detokenize(code);
+
+        if (token == null)
+        {
+            return new BadRequestObjectResult("Bad Request, Github request error");
+        }
+
+        return new OkObjectResult(token.AccessToken);
+    }
+}
