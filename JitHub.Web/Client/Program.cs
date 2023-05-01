7bff5cb2837af34916d20845e3a5844533d574d6
nerocui <nerocui@outlook.com>
2023-04-30 23:13:31 -0700

    Revert "testing api key (#40)"
    This reverts commit 5cd361468aa738e1177d9b48e90b0f25b28f04c7.

--- JitHub.Web/Client/Program.cs
+++ JitHub.Web/Client/Program.cs
@@ -1,5 +1,4 @@
 using JitHub.Web;
-using JitHub.Web.Shared;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 namespace JitHub.Web
@@ -11,7 +10,7 @@ namespace JitHub.Web
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
-            Config.API_KEY = "KqMwH3gSmD711FcrnsUQP5aXS1L8ksfDsP_YOdyEAvBdAzFuSTxUjg==";
+
             builder.Services.AddScoped(sp =>
                 new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
             await builder.Build().RunAsync();
